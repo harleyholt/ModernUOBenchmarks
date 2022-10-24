d6f8dabf3c813937e65874ea82004701be0153c4
Harley Holt <holt.harley@gmail.com>
2022-10-23 17:24:33 -0700

    Benchmarks for #1067

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using Server;
+
+namespace ModernUOBenchmarks;
+
+[MemoryDiagnoser]
+public class ToStringBenchmarks
+{
+    private Point2D[] points;
+
+    public ToStringBenchmarks() {
+        points = new Point2D[] {
+            new Server.Point2D(1, 1),
+            new Server.Point2D(2, 2),
+            new Server.Point2D(3, 3)
+        };
+    }
+
+    [Benchmark]
+    public string CallToString() {
+        return points[0].ToString();
+    }
+
+    [Benchmark]
+    public string InterpolatedString() {
+        return $"{points[0]}";
+    }
+
+    [Benchmark]
+    public string InterpolatedStringMultiplePoints() {
+        return $"{points[0]} {points[1]} {points[2]}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<ToStringBenchmarks>();
+    }
+}
